6d188495e850504427f35b4a59aaea860a227ff5
leoalfonseca <leonardo.fonseca@codgital.com>
2026-03-02 14:34:02 -0300

    feature: Github Actions deploy

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { toast } from 'react-toastify';
 import { useRouter } from 'next/router';
 import { api } from 'services/api';
 
@@ -22,19 +21,13 @@ const AuthContext = React.createContext({} as IAuthContext);
 const AuthProvider: React.FC<IAuthProvider> = ({ children }) => {
   const router = useRouter();
 
-
   const signIn = async (dataLogin: AuthProps) => {
     try {
-
-      toast.success('Usuário logado com sucesso!');
-
       router.push('/home');
     } catch (error: any) {
-      toast.error('Usuário ou senha inválidos');
       throw error;
     }
   };
-
 
   const signOut = async () => {
     try {
